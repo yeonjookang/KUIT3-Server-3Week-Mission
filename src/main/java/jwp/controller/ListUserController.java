287c71c8c1ec478840f5e1b2acc09c44e4803753
kang yeon joo <rkdduswndi@naver.com>
2024-04-02 16:35:24 +0900

    feat: 요구사항 1-2 구현

--- src/main/java/jwp/controller/ListUserController.java
+++ src/main/java/jwp/controller/ListUserController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/user/userList")
+@WebServlet("/user/list")
 public class ListUserController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
